08097d7d6494b622e8815738f1b734ff2deee662
suryaprasath07 <63159918+suryaprasath07@users.noreply.github.com>
2021-04-05 23:27:53 +0530

    Update final_project_2.c

--- final_project_2.c
+++ final_project_2.c
@@ -5,31 +5,36 @@
 int main(int argc, char *argv[])
 {
     struct dirent *de; // Pointer for directory entry
-    char s[100];
+    char s[100];       //for storing the directory name which is passed as argument
+    //assingning value to s according to condition
     if (argc == 2)
     {
         strcpy(s, argv[1]);
     }
-    else
+    else if (argc == 1)
     {
         strcpy(s, ".");
     }
+    else
+    {
+        return -1;
+    }
+    //opening the directory
     DIR *dr = opendir(s);
     if (dr == NULL) // opendir returns NULL if couldn't open directory
     {
-        printf("Could not open current directory");
-        return 0;
+        return -1;
     }
     int directories = 0;
     int files = 0;
     while ((de = readdir(dr)) != NULL)
     {
-        if (de->d_type == 8)
+        if (de->d_type == 8) //d_type=8 then it is filetype
         {
             files++;
             printf("\nFile %d : %s", files, de->d_name);
         }
-        else
+        else if (de->d_type == 4) //d_type=4 then it is directory
         {
             directories++;
             printf("\nDirectory %d : %s", directories, de->d_name);
