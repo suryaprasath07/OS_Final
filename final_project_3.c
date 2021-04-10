b98472df5a237ea0c04893c752523b6e3e43abda
suryaprasath07 <63159918+suryaprasath07@users.noreply.github.com>
2021-04-10 19:08:45 +0530

    Update final_project_3.c

--- final_project_3.c
+++ final_project_3.c
@@ -2,25 +2,14 @@
 #include <dirent.h>
 #include <string.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
     struct dirent *de; // Pointer for directory entry
-    char s[100];       //for storing the directory name which is passed as argument
+    char s[100];
+    getcwd(s,sizeof(s));       //for storing the directory name which is passed as argument
     int flag = 0;      //to check if there isn't any file in the directory
-    //assingning value to s according to condition
-    if (argc == 2)
-    {
-        strcpy(s, argv[1]);
-    }
-    else if (argc == 1)
-    {
-        strcpy(s, ".");
-    }
-    else
-    {
-        return -1;
-    }
     //opening the directory
     DIR *dr = opendir(s);
     if (dr == NULL) // opendir returns NULL if couldn't open directory
@@ -30,10 +19,10 @@ int main(int argc, char *argv[])
     while ((de = readdir(dr)) != NULL)
     {
         int fd = open(de->d_name, O_RDONLY); //Open for reading only.
-        int size = lseek(fd, 0, SEEK_END);   //the file's offset is set to the size of the file plus the offset(0)
         if (de->d_type == 8)                 //d_type=8 then it is filetype
         {
-            printf("\name : %s size : %d", de->d_name, size);
+            int size = lseek(fd, 0, SEEK_END);   //the file's offset is set to the size of the file plus the offset(0)
+            printf("\n Name : %s size : %d", de->d_name, size);
             flag = 1;
         }
         close(fd);
